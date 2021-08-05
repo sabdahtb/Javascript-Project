87672fdd643492740037fca01d811f05878efa93
Sabda Hutabarat <sabdahutabarat2804@gmail.com>
2021-08-05 18:53:20 +0700

    Mengedit JS

--- 2.Hitung MajuMundur/script.js
+++ 2.Hitung MajuMundur/script.js
@@ -2,16 +2,37 @@
 //?  MEMANGGIL ELEMENT HTML
 //? ===========================
 
-let nomor = 0;
 const tampil = document.querySelector('.nomor');
-const btns = document.querySelectorAll('.btn')
-
+const btns = document.querySelectorAll('.btn');
+const mundur = document.querySelector('.mundur');
+const reset = document.querySelector('.reset');
+const maju = document.querySelector('.maju')
 //? ===========================
 //?  MEMBUAT FUNCTION TOMBOL
 //? ===========================
 
-const mundur = document.querySelector('.mundur');
+let nomor = 0;
+
 mundur.addEventListener('click',function(){
     nomor--;
-    tampil.textContent = nomor;
-})
+    tampil.innerHTML=nomor;
+    tampil.style.color='red'
+});
+if(nomor<0){
+    tampil.style.color='red'
+}
+
+reset.addEventListener('click',function(){
+    nomor=0;
+    tampil.innerHTML=nomor;
+});
+
+maju.addEventListener('click',function(){
+    nomor++;
+    tampil.innerHTML=nomor;
+});
+
+
+if(nomor>0){
+    tampil.style.color='green'
+}
