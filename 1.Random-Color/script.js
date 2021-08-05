d3d6eb832fd462bbeb855001f79aacddbba4f803
Sabda Hutabarat <sabdahutabarat2804@gmail.com>
2021-08-05 13:35:52 +0700

    Mengedit file html css dan js

---
+++ 1.Random-Color/script.js
@@ -0,0 +1,25 @@
+//?================================
+//? Membuat pengaturan warna
+//?================================
+//? contoh background color rgb(122,122,122)
+
+const tombol = document.querySelector('.btn');
+
+tombol.addEventListener('click',function(){
+
+    const merah = Math.floor(Math.random()*255);
+    const hijau = Math.floor(Math.random()*255);
+    const biru = Math.floor(Math.random()*255);
+
+    document.body.style.backgroundColor=`rgb(${merah},${hijau},${biru})`;
+
+    //? ================================
+    //? Menampilkan pengaturan warna dibawah tombol
+    //? ================================
+
+    let tampil = document.querySelector('.output');
+
+    tampil.textContent=`rgb(${merah}, ${hijau}, ${biru})`
+
+    return tombol;
+});
